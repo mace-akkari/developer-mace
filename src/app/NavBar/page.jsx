d2c3fe830713890dc754bfea5f3fba668a6dd9a0
Mace Akkari <mace.akkari@relyfe.com>
2024-05-30 19:26:28 +0100

    add responsive burger menu

--- src/app/NavBar/page.jsx
+++ src/app/NavBar/page.jsx
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import navList from "./NavList";
 import { BiMenu } from "react-icons/bi";
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
   return (
     <nav className="fixed w-full z-50 justify-between items-center flex">
       <div className="h-20 flex items-center flex-shrink-0">
@@ -23,7 +26,28 @@ const Navbar = () => {
           </li>
         ))}
       </menu>
-      <BiMenu className="text-3xl md:hidden m-5" />
+      <BiMenu
+        onClick={() => setOpen((prevOpen) => !prevOpen)}
+        className={`z-50 ${
+          open ? "text-background-blue" : "text-custom-dark-blue"
+        } text-3xl md:hidden m-5 transform transition-transform duration-300 hover:scale-150`}
+      />
+      <menu
+        className={`flex flex-col justify-center gap-10 items-center text-lg md:hidden absolute w-2/3 h-screen font-medium bg-custom-dark-blue top-0 duration-300 ${
+          open ? "right-0" : "right-full"
+        }`}
+      >
+        {navList?.map((list, index) => (
+          <li key={index} onClick={() => setOpen(false)}>
+            <a
+              href={list?.link}
+              className="inline-block text-background-blue hover:text-white transform transition-transform duration-300 hover:scale-150"
+            >
+              {list?.name}
+            </a>
+          </li>
+        ))}
+      </menu>
     </nav>
   );
 };

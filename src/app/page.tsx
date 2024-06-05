a63e68bdfd71f0632ae887e55cb253bc771fce66
Mace Akkari <mace.akkari@relyfe.com>
2024-06-05 21:38:01 +0100

    add about me section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@
 
 import Navbar from "./NavBar/page";
 import IntroSection from "./IntroSection/page";
+import About from "./AboutSection/page";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
       <IntroSection />
+      <About />
     </div>
   );
 }

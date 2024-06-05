8f594d033242b9259c412aeaab08957de5876631
Mace Akkari <mace.akkari@relyfe.com>
2024-06-05 21:38:13 +0100

    add about me section

---
+++ src/app/AboutSection/page.tsx
@@ -0,0 +1,15 @@
+import AboutMe from "./aboutMe";
+
+const About = () => {
+  return (
+    <section id="about" className="pb-10 text-custom-dark-blue">
+      <div className=" text-4xl font-semibold mt-8 flex justify-center gap-2">
+        <h3>About</h3>
+        <h3 className="text-custom-dark-blue">Me</h3>
+      </div>
+      <AboutMe />
+    </section>
+  );
+};
+
+export default About;

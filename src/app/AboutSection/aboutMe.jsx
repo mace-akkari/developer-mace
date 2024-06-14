65da1ef74670b5718f2695e1aed2879a383460a5
Mace Akkari <mace.akkari@relyfe.com>
2024-06-14 10:40:12 +0100

    update aboutSection

---
+++ src/app/AboutSection/aboutMe.jsx
@@ -0,0 +1,19 @@
+const AboutMe = () => {
+  return (
+    <div className="flex flex-col items-center md:gap-6 gap-12 max-w-6xl mx-auto text-custom-dark-blue mt-5">
+      <p className="text-justify leading-7 w-11/12 mx-auto text-base md:text-lg lg:text-xl">
+        As a passionate and experienced Full Stack Developer, I specialise in
+        user-centric solutions and workflow optimization. Proficient in
+        JavaScript, React, TypeScript, and Node.js, I excel in feature
+        development, bug fixing, and enhancing platform performance. I have
+        successfully collaborated on diverse projects in recruitment platforms,
+        streetwear rental apps, and medical startups. Effective communication
+        and teamwork are central to my approach, ensuring seamless collaboration
+        and project success. Dedicated to continuous learning and staying ahead
+        of technological advancements.
+      </p>
+    </div>
+  );
+};
+
+export default AboutMe;

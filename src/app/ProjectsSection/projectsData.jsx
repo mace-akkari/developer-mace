b63864302e320de85bcaeb2fa4e6aa1696cf1b41
Mace Akkari <mace.akkari@relyfe.com>
2024-06-27 13:09:57 +0100

    add project imgs data

---
+++ src/app/ProjectsSection/projectsData.jsx
@@ -0,0 +1,28 @@
+import theMartian from "/public/images/projects/the-martian.png";
+
+export const projects = [
+  {
+    img: theMartian,
+    name: "The Martian",
+    github_link: "https://github.com/mace-akkari/The-Martian",
+    demo_link: "https://the-martian.vercel.app/",
+  },
+  {
+    img: theMartian,
+    name: "The Martian",
+    github_link: "https://github.com/mace-akkari/The-Martian",
+    demo_link: "https://the-martian.vercel.app/",
+  },
+  {
+    img: theMartian,
+    name: "The Martian",
+    github_link: "https://github.com/mace-akkari/The-Martian",
+    demo_link: "https://the-martian.vercel.app/",
+  },
+  {
+    img: theMartian,
+    name: "The Martian",
+    github_link: "https://github.com/mace-akkari/The-Martian",
+    demo_link: "https://the-martian.vercel.app/",
+  },
+];

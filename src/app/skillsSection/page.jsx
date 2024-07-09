2a800be8e459bab7cf61d35236e756904395ea5d
Mace Akkari <mace.akkari@relyfe.com>
2024-07-09 19:30:45 +0100

    add padding top

--- src/app/skillsSection/page.jsx
+++ src/app/skillsSection/page.jsx
@@ -5,12 +5,12 @@ const Skills = () => {
   return (
     <section
       id="skills"
-      className="py-10 bg-custom-dark-blue mt-8 text-background-blue text-center"
+      className="pb-10 pt-24 bg-custom-dark-blue mt-8 text-background-blue text-center"
     >
       <div className="flex justify-center text-4xl font-semibold gap-2">
         <h3 className="text-background-blue">My Skills</h3>
       </div>
-      <ul className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-6 lg:grid-cols-8 gap-10 mt-8">
+      <ul className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-6 gap-10 mt-8">
         {skills?.map((skill, index) => (
           <li key={index} className="flex flex-col items-center">
             <div className="text-5xl">{skill.icon}</div>

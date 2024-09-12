76b7f243961de1fa5383de3c9e98f18f7e608118
Mace Akkari <mace.akkari@relyfe.com>
2024-09-12 14:39:46 +0100

    rename path

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import Navbar from "./NavBar/page.jsx";
-import IntroSection from "./IntroSection/page.jsx";
-import About from "./AboutSection/page";
-import Skills from "./SkillsSection/page.jsx";
-import Project from "./ProjectsSection/page.jsx";
-import Footer from "./Footer/page.jsx";
+import Navbar from "./NavBar/index.jsx";
+import IntroSection from "./IntroSection/index.jsx";
+import About from "./AboutSection/index.jsx";
+import Skills from "./SkillsSection/index.jsx";
+import Project from "./ProjectsSection/index.jsx";
+import Footer from "./Footer/index.jsx";
 
 export default function Home() {
   return (
